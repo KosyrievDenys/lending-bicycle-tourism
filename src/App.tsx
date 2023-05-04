30ee5efb2611ba02c6c2a684b4defeee43c181b5
denys_kosyriev <utross2241@gmail.com>
2023-05-05 00:26:03 +0300

    feature-lending-bicycle-tourism

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+//libs
+import cn from 'classnames';
+import { Swiper, SwiperSlide } from "swiper/react";
+import 'swiper/css';
+
+//assets
+import './App.module.scss';
+import arrow from './assets/images/icons/arrow.svg';
+import arrowLong from './assets/images/icons/arrow-long.svg';
+import bicycleIcon from './assets/images/icons/bicycle.svg';
+import banner from './assets/images/banner.jpg';
+import features from './assets/images/features.jpg';
+import reviews from './assets/images/reviews.jpg';
+import bicycle from './assets/images/bicycle.png';
+import formBack from './assets/images/form-back.png';
+import heart from './assets/images/heart.png';
+import price from './assets/images/price.png';
+import reviewsBack from './assets/images/reviews-back.png';
+
+export const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className='container'></div>
       </header>
+      <main>
+
+      </main>
     </div>
   );
 }
 
-export default App;
